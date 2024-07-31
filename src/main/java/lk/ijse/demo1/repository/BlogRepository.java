67168d6b43e3960174098a339056eff8ecb559a0
danukarangith <danukarangith456@gmail.com>
2024-07-31 13:30:52 +0530

    add repositopry anotation

--- src/main/java/lk/ijse/demo1/repository/BlogRepository.java
+++ src/main/java/lk/ijse/demo1/repository/BlogRepository.java
@@ -1,6 +1,8 @@
 package lk.ijse.demo1.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BlogRepository extends JpaRepository {
 }
