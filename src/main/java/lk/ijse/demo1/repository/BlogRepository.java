57b0eebefbe03af90bf6363a2df734659f785e4e
danukarangith <danukarangith456@gmail.com>
2024-07-31 13:40:07 +0530

    add data type

--- src/main/java/lk/ijse/demo1/repository/BlogRepository.java
+++ src/main/java/lk/ijse/demo1/repository/BlogRepository.java
@@ -1,8 +1,9 @@
 package lk.ijse.demo1.repository;
 
+import lk.ijse.demo1.entity.Blog;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface BlogRepository extends JpaRepository {
+public interface BlogRepository extends JpaRepository<Blog,Integer> {
 }
